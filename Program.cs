0afed13c7e60c6908ea312e18f4da4b184d60732
kalakal <sayedfaizanqadri@gmail.com>
2022-12-03 17:09:50 +0530

    assigmnmentproblem added

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Contact contact = new Contact()
+            {
+                FirstName = "Sayed",
+                LastName = "Faizan",
+                Address = "Jammu",
+                City = "Jammu",
+                State = "Jammu and Kashmir",
+                Zip = 185212,
+                PhoneNumber = 962278,
+                Email = "sayedfaizanqadri@gmail.com",
+            };
+            Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.Zip + "\n" + contact.PhoneNumber + "\n" + contact.Email);
+        }
+    }
+}
